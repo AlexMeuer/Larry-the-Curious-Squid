c03b6cfc4fa6659ea7fa6420aa1f944142a3da76
Alison <alsunnid@gmail.com>
2014-10-20 15:40:40 +0100

    BlackHole.h added, Block.h added, Player.h added

---
+++ include/BlackHole.h
@@ -0,0 +1,12 @@
+#ifndef _BLACKHOLE_
+#define _BLACKHOLE_
+
+#include "GameEntity.h"
+#include "Force.h"
+
+class BlackHole: GameEntity {
+private:
+	Force m_force;
+};
+
+#endif
---
+++ include/Player.h
@@ -0,0 +1,12 @@
+#ifndef _PLAYER_
+#define _PLAYER_
+
+#include "Force.h"
+
+class Player {
+private:
+	Force m_force;
+public:
+	void Update();
+};
+#endif
