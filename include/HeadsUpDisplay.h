5d07a9eac3d01ccd822695e0eac1e7bdfd788664
Alison <alsunnid@gmail.com>
2014-12-02 15:40:58 +0000

    HUD header has been filled a bit

--- include/HeadsUpDisplay.h
+++ include/HeadsUpDisplay.h
@@ -1,6 +1,14 @@
 #ifndef _HEADSUPDISPLAY_
 #define _HEADSUPDISPLAY_
 
+class HeadsUpDisplay {
+private:
+	static HeadsUpDisplay *m_instance;
+	HeadsUpDisplay() {}
+public:
+	~HeadsUpDisplay() {}
 
+	static HeadsUpDisplay* instance() { return instance == NULL ? m_instance = new HeadsUpDisplay() : m_instance; }
 
+};
 #endif
--- src/HeadsUpDisplay.cpp
+++ src/HeadsUpDisplay.cpp
@@ -1,2 +1,4 @@
 #include "../include/HeadsUpDisplay.h"
 
+HeadsUpDisplay* HeadsUpDisplay::m_instance = NULL;
+
