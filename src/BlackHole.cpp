a9b3564d4e0fe248be7d7f75247c6b570ab08aac
Alison <alsunnid@gmail.com>
2014-12-15 13:55:53 +0000

    fixed blackhole constructor

--- src/BlackHole.cpp
+++ src/BlackHole.cpp
@@ -1,7 +1,7 @@
 #include "BlackHole.h"
 
 BlackHole::BlackHole(Texture* texture, Vector2f position, Vector2f velocity, Vector2f scale, float angularVel, float rotation_degrees, float mass) 
-	: GameEntity( texture,  position, velocity, scale,  angularVel,  rotation_degrees,  mass) , m_force(Vector2f(571.5, 471.5), -200){
+	: GameEntity( texture,  position, velocity, scale,  angularVel,  rotation_degrees,  mass) , m_force(Vector2f(position.x + this->getSprite().getGlobalBounds().width/2.0f,position.y + this->getSprite().getGlobalBounds().height/2.0f), -200){
 		m_sprite.setPosition(position.x, position.y);
 		m_sprite.setOrigin(71.5f, 71.5f);
 }
